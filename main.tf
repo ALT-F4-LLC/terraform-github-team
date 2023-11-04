2abee4b44ba72ee87a57afa4267ca9599a4c8364
Erik Reinert <4638629+erikreinert@users.noreply.github.com>
2023-11-04 12:52:21 -0700

    feat(*): upgrade provider to 5.41.0

--- main.tf
+++ main.tf
@@ -1,8 +1,13 @@
-terraform {
-  required_providers {
-    github = {
-      source  = "integrations/github"
-      version = "5.37.0"
-    }
-  }
+resource "github_team" "self" {
+  name        = var.name
+  description = var.description
+  privacy     = var.privacy
+}
+
+resource "github_team_membership" "self" {
+  for_each = var.members
+
+  role     = each.value
+  team_id  = github_team.self.id
+  username = each.key
 }
